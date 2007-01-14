1f2beddd995b28c519c821f613961b6f0be27f4e
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2007-01-14 13:07:06 +0000

    new subdir packager, changed filelist generation sorry Ralf
    svn path=/trunk/kdesupport/kdewin32/installer/; revision=623212

---
+++ packager/main.cpp
@@ -0,0 +1,74 @@
+/****************************************************************************
+**
+** Copyright (C) 2007  Christian Ehrlicher <ch.ehrlicher@gmx.de>.
+** All rights reserved.
+**
+** This file is part of the KDE installer for windows
+**
+** This file may be used under the terms of the GNU General Public
+** License version 2.0 as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL included in the packaging of
+** this file.  Please review the following information to ensure GNU
+** General Public Licensing requirements will be met:
+** http://www.trolltech.com/products/qt/opensource.html
+**
+** If you are unsure which license is appropriate for your use, please
+** review the following information:
+** http://www.trolltech.com/products/qt/licensing.html or contact the
+** sales department at sales@trolltech.com.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#include <QtDebug>
+#include <QCoreApplication>
+#include <QFileInfo>
+
+#include "packager.h"
+
+static void printHelp(const QString &addInfo)
+{
+    // too lazy atm 
+    
+    exit(1);    
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+
+    QStringList args = app.arguments();
+    QString root, version;
+    QFileInfo fi;
+    
+    int idx = args.indexOf("--root");
+    if(idx != -1 && idx < args.count() -1) {
+        root = args[idx + 1];
+        fi = QFileInfo(root);
+    }
+
+    idx = args.indexOf("--version");
+    if(idx != -1 && idx < args.count() -1)
+        version = args[idx + 1];
+
+    if(root.isEmpty())
+       printHelp("--root not specified");
+    if(version.isEmpty())
+       printHelp("--version not specified");
+    
+    if(!fi.isDir() || !fi.isReadable())
+       printHelp(QString("Root path %1 is not accessible").arg(root));
+    
+
+    Packager packager;
+    // packager.generatePackageFileList()
+    
+    packager.generateFileList(fi.filePath(), "*.*", "*.bak;manifest");
+    qDebug() << packager.fileList().join("\n");
+    packager.makePackage(fi.filePath(),"dbus","1.0.1");
+
+    return 0;
+}
+
