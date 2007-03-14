b90c2700b987026c43464461f7f508779b5927c1
Ralf Habacker <ralf.habacker@freenet.de>
2007-03-14 12:35:21 +0000

    copyright update
    svn path=/trunk/kdesupport/kdewin32/installer/; revision=642444

--- shared/package.h
+++ shared/package.h
@@ -1,23 +1,22 @@
 /****************************************************************************
 **
-** Copyright (C) 2005 Ralf Habacker. All rights reserved.
+** Copyright (C) 2005-2007 Ralf Habacker. All rights reserved.
 **
 ** This file is part of the KDE installer for windows
 **
-** This file may be used under the terms of the GNU General Public
-** License version 2.0 as published by the Free Software Foundation
-** and appearing in the file LICENSE.GPL included in the packaging of
-** this file.  Please review the following information to ensure GNU
-** General Public Licensing requirements will be met:
-** http://www.trolltech.com/products/qt/opensource.html
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Library General Public
+** License version 2 as published by the Free Software Foundation.
 **
-** If you are unsure which license is appropriate for your use, please
-** review the following information:
-** http://www.trolltech.com/products/qt/licensing.html or contact the
-** sales department at sales@trolltech.com.
+** This library is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+** Library General Public License for more details.
 **
-** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+** You should have received a copy of the GNU Library General Public License
+** along with this library; see the file COPYING.LIB.  If not, write to
+** the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+** Boston, MA 02110-1301, USA.
 **
 ****************************************************************************/
 #ifndef PACKAGE_H
