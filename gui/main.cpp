6f64b1726468c8f55853ad3af636f1ba8024fe20
Ralf Habacker <ralf.habacker@freenet.de>
2007-04-04 20:29:21 +0000

    - install message output handler to save all logs into a file, does not produce output in release build yet
    svn path=/trunk/kdesupport/kdewin-installer/; revision=650561

--- gui/main.cpp
+++ gui/main.cpp
@@ -4,11 +4,45 @@
 ****************************************************************************/
 
 #include <QApplication>
+#include <QFile>
 
 #include "installwizard.h"
 
+QFile *log; 
+
+void myMessageOutput(QtMsgType type, const char *msg)
+ {
+    switch (type) {
+     case QtDebugMsg:
+         log->write("Debug:");
+         log->write(msg);
+         log->write("\n");
+         break;
+     case QtWarningMsg:
+         log->write("Warning:");
+         log->write(msg);
+         log->write("\n");
+         break;
+     case QtCriticalMsg:
+         log->write("Critical:");
+         log->write(msg);
+         log->write("\n");
+         break;
+     case QtFatalMsg:
+         log->write("Fatal:");
+         log->write(msg);
+         log->write("\n");
+         abort();
+     }
+ }
+
 int main(int argc, char *argv[])
 {
+    QFile f("kdewin-installer.log");
+    log = &f;
+    f.open(QIODevice::WriteOnly);
+
+    qInstallMsgHandler(myMessageOutput);
     QApplication app(argc, argv);
 
     InstallWizard *wizard = new InstallWizard();
