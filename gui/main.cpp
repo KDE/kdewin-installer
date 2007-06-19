8de86bd719614b3a38bd4c5714042684aeeecebf
Ralf Habacker <ralf.habacker@freenet.de>
2007-06-19 08:13:20 +0000

    - used shared qt message handler setter
    svn path=/trunk/kdesupport/kdewin-installer/; revision=677496

--- gui/main.cpp
+++ gui/main.cpp
@@ -4,54 +4,17 @@
 ****************************************************************************/
 
 #include <QApplication>
-#include <QFile>
-#include <QDateTime>
 
 #include "installwizard.h"
+#include "misc.h"
 
 // FIXME: required by uninstaller progress
 InstallWizard *wizard;
 
-QFile *log; 
-
-void myMessageOutput(QtMsgType type, const char *msg)
- {
-    log->write(QDateTime::currentDateTime().toString("[yyyy-MM-dd hh:mm:ss] ").toLocal8Bit().data());
-    switch (type) {
-     case QtDebugMsg:
-         log->write("Debug:");
-         log->write(msg);
-         log->write("\n");
-         log->flush();
-         break;
-     case QtWarningMsg:
-         log->write("Warning:");
-         log->write(msg);
-         log->write("\n");
-         log->flush();
-         break;
-     case QtCriticalMsg:
-         log->write("Critical:");
-         log->write(msg);
-         log->write("\n");
-         log->flush();
-         break;
-     case QtFatalMsg:
-         log->write("Fatal:");
-         log->write(msg);
-         log->write("\n");
-         log->flush();
-         abort();
-     }
- }
 
 int main(int argc, char *argv[])
 {
-    QFile f("kdewin-installer.log");
-    log = &f;
-    f.open(QIODevice::WriteOnly);
-
-    qInstallMsgHandler(myMessageOutput);
+    setMessageHandler();
     QApplication app(argc, argv);
 
     wizard = new InstallWizard();
