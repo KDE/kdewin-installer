80b172bf97f31a624d3742f738b2ed132fa3cf35
Ralf Habacker <ralf.habacker@freenet.de>
2007-10-17 13:36:46 +0000

    tried to improve the indention of debug messages
    svn path=/trunk/kdesupport/kdewin-installer/; revision=726307

--- config.h.cmake
+++ config.h.cmake
@@ -6,15 +6,24 @@
 
 #include <QString>
 #include <QtDebug>
+inline QString &DUMP_INDENT(int inc = 0)
+{
+    static int dumpLevel = 0;
+    static QString l="";
+    dumpLevel += inc;
+    return l.fill(' ',dumpLevel);
+}
 
 inline void DUMP_HEADER(const QString &title, const QString &_class=QString())
 {
-    qDebug() << "-------------" << title << _class << "---(Start)------";
+//    qDebug() << "-------------" << title << _class << "---(Start)------";
+    qDebug() << DUMP_INDENT(1) << title << _class ;
 }    
     
 inline void DUMP_FOOTER(const QString &title, const QString &_class=QString())
 {
-    qDebug() << "-------------" << title << _class << "---(End)--------";
+//    qDebug() << "-------------" << title << _class << "---(End)--------";
+      qDebug() << DUMP_INDENT(-1);
 }
 
 #endif
