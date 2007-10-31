03c60c9383e107ffc07eba184a9ce3e3f6408b2c
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2007-10-31 16:24:45 +0000

    upx is also available on linux
    svn path=/trunk/kdesupport/kdewin-installer/; revision=731407

--- cmake/modules/FindUPX.cmake
+++ cmake/modules/FindUPX.cmake
@@ -1,42 +1,40 @@
 #
-# find UPX 
-# 
+# find UPX
+#
 # Copyright (c) 2006, Ralf Habacker
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 #
 
-if (WIN32)
-    if(NOT UPX_EXECUTABLE)
-    	file(TO_CMAKE_PATH "$ENV{PROGRAMFILES}" _progFiles)
-        find_program(UPX_EXECUTABLE upx
-            PATHS
-                ${_progFiles}/upx
-                ${_progFiles}/upx/bin
-        )
+if(NOT UPX_EXECUTABLE)
+  file(TO_CMAKE_PATH "$ENV{PROGRAMFILES}" _progFiles)
+  find_program(UPX_EXECUTABLE upx
+    PATHS
+      ${_progFiles}/upx
+      ${_progFiles}/upx/bin
+  )
 
-        if (UPX_EXECUTABLE)        
-            set(UPX_FOUND TRUE)
-            execute_process(
-                COMMAND ${UPX_EXECUTABLE} -V
-                OUTPUT_VARIABLE output
-                ERROR_VARIABLE output
-            )
-            STRING(REGEX MATCH "upx [0-9]+\\.[0-9]+" upx_version "${output}")
-            STRING(REGEX MATCH "LZMA [a-zA-Z ]+[0-9]+\\.[0-9]+"  UPX_LZMA_VERSION "${output}")
-        endif (UPX_EXECUTABLE)        
-    else(NOT UPX_EXECUTABLE)
-       set(UPX_FOUND)
-    endif(NOT UPX_EXECUTABLE)
-    
-    if (UPX_FOUND)
-      if (NOT UPX_FIND_QUIETLY)
-        message(STATUS "Found ${upx_version} ${UPX_LZMA_VERSION} ")
-      endif (NOT UPX_FIND_QUIETLY)
-    else (UPX_FOUND)
-      if (UPX_FIND_REQUIRED)
-        message(FATAL_ERROR "Could NOT find UPX program")
-      endif (UPX_FIND_REQUIRED)
-    endif (UPX_FOUND)
-endif (WIN32)
+  if (UPX_EXECUTABLE)
+    set(UPX_FOUND TRUE)
+    execute_process(
+        COMMAND ${UPX_EXECUTABLE} -V
+        OUTPUT_VARIABLE output
+        ERROR_VARIABLE output
+    )
+    STRING(REGEX MATCH "upx [0-9]+\\.[0-9]+" upx_version "${output}")
+    STRING(REGEX MATCH "LZMA [a-zA-Z ]+[0-9]+\\.[0-9]+"  UPX_LZMA_VERSION "${output}")
+  endif (UPX_EXECUTABLE)
+else(NOT UPX_EXECUTABLE)
+  set(UPX_FOUND)
+endif(NOT UPX_EXECUTABLE)
+
+if (UPX_FOUND)
+  if (NOT UPX_FIND_QUIETLY)
+    message(STATUS "Found ${upx_version} ${UPX_LZMA_VERSION} ")
+  endif (NOT UPX_FIND_QUIETLY)
+else (UPX_FOUND)
+  if (UPX_FIND_REQUIRED)
+    message(FATAL_ERROR "Could NOT find UPX program")
+  endif (UPX_FIND_REQUIRED)
+endif (UPX_FOUND)
 
