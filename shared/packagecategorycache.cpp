e0e672d17306263623be23ef164ef42c1d1f93a5
Ralf Habacker <ralf.habacker@freenet.de>
2007-11-14 23:34:20 +0000

    added class for caching package categories and related packages, this may be used for generic package filtering in the future.
    Note: In the first try I used an QHash <QString,CategoryListType> where CategoryListType contains a QString and a QList<Package *>, but unfortunally the added package pointer are corrupted on access :-( 
    
    svn path=/trunk/kdesupport/kdewin-installer/; revision=736833

---
+++ shared/packagecategorycache.cpp
@@ -0,0 +1,67 @@
+/****************************************************************************
+**
+** Copyright (C) 2005-2007 Ralf Habacker. All rights reserved.
+**
+** This file is part of the KDE installer for windows
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Library General Public
+** License version 2 as published by the Free Software Foundation.
+**
+** This library is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+** Library General Public License for more details.
+**
+** You should have received a copy of the GNU Library General Public License
+** along with this library; see the file COPYING.LIB.  If not, write to
+** the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+** Boston, MA 02110-1301, USA.
+**
+****************************************************************************/
+
+#include "packagecategorycache.h"
+
+PackageCategoryCache::PackageCategoryCache()
+{
+}
+
+void PackageCategoryCache::setNote(const QString &category, const QString &notes)
+{
+    m_data[category].notes = notes;
+}
+
+void PackageCategoryCache::addPackage(Package *pkg)
+{
+    foreach(QString category, pkg->categories())
+        m_data[category].packages.append(pkg->name());
+}
+
+QStringList PackageCategoryCache::categories()
+{
+    QStringList keys;
+    QMap<QString, CategoryListType>::iterator i = m_data.begin();
+    for (;i != m_data.end(); ++i) 
+    {
+        // @TODO fix me empoty categories should not be added
+        if (!i.key().isEmpty())
+            keys << i.key() + ":" + m_data[i.key()].notes;
+    }
+    return keys;
+}
+
+QList <Package *>PackageCategoryCache::packages(const QString &category, PackageList &list)
+{
+    QList <Package *> packages;
+    if (!m_data.contains(category))
+        return packages;
+    foreach(QString pkgName, m_data[category].packages) 
+    {
+        Package *p = list.getPackage(pkgName);
+        if (p)
+            packages.append(p);
+    }
+    return packages;
+}
+
+PackageCategoryCache categoryCache;
---
+++ shared/packagecategorycache.h
@@ -0,0 +1,53 @@
+/****************************************************************************
+**
+** Copyright (C) 2005-2007 Ralf Habacker. All rights reserved.
+**
+** This file is part of the KDE installer for windows
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Library General Public
+** License version 2 as published by the Free Software Foundation.
+**
+** This library is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+** Library General Public License for more details.
+**
+** You should have received a copy of the GNU Library General Public License
+** along with this library; see the file COPYING.LIB.  If not, write to
+** the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+** Boston, MA 02110-1301, USA.
+**
+****************************************************************************/
+#ifndef PACKAGECATEGORYCACHE_H
+#define PACKAGECATEGORYCACHE_H
+
+#include <QString>
+#include <QStringList>
+#include <QList>
+
+#include "package.h"
+#include "packagelist.h"
+
+class PackageCategoryCache 
+{
+    public:
+        // @TODO save category notes too from sites            
+        typedef struct {
+            QString notes;
+            QList<QString> packages;
+        } CategoryListType;
+        PackageCategoryCache();
+        void addPackage(Package *pkg);
+        void setNote(const QString &category, const QString &notes);
+        QStringList categories();
+        QList <QString>packages(const QString &category) { return m_data[category].packages; }
+        QList <Package *>packages(const QString &category, PackageList &list);
+
+    private:
+        QMap<QString,CategoryListType> m_data;
+};
+
+extern PackageCategoryCache categoryCache;
+
+#endif
