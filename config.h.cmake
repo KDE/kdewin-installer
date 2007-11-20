f80f5dd0fd1e72e024d8823b1defde60999a7baf
Ralf Habacker <ralf.habacker@freenet.de>
2007-11-20 07:29:28 +0000

    added BZIP2_UNPACK_SUPPORT
    svn path=/trunk/kdesupport/kdewin-installer/; revision=739003

--- config.h.cmake
+++ config.h.cmake
@@ -3,27 +3,6 @@
 #cmakedefine VERSION "@VERSION@"
 #cmakedefine CMAKE_SOURCE_DIR "@CMAKE_SOURCE_DIR@"
 #cmakedefine SEVENZIP_UNPACK_SUPPORT 1
-
-#include <QString>
-#include <QtDebug>
-inline QString &DUMP_INDENT(int inc = 0)
-{
-    static int dumpLevel = 0;
-    static QString l="";
-    dumpLevel += inc;
-    return l.fill(' ',dumpLevel);
-}
-
-inline void DUMP_HEADER(const QString &title, const QString &_class=QString())
-{
-//    qDebug() << "-------------" << title << _class << "---(Start)------";
-    qDebug() << DUMP_INDENT(1) << title << _class ;
-}    
-    
-inline void DUMP_FOOTER(const QString &title, const QString &_class=QString())
-{
-//    qDebug() << "-------------" << title << _class << "---(End)--------";
-      qDebug() << DUMP_INDENT(-1);
-}
+#cmakedefine BZIP2_UNPACK_SUPPORT 1
 
 #endif
