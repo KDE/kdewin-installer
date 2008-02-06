22bc5f636503975f69643d3d80a7d5612d228481
Ralf Habacker <ralf.habacker@freenet.de>
2008-02-06 14:05:52 +0000

    - added doc - cleanup up script name generating
    svn path=/trunk/kdesupport/kdewin-installer/; revision=771603

--- cmake/modules/Win32Macros.cmake
+++ cmake/modules/Win32Macros.cmake
@@ -1,7 +1,7 @@
 #
 # win32 macros 
 # 
-# Copyright (c) 2006-2007, Ralf Habacker
+# Copyright (c) 2006-2007, Ralf Habacker <ralf.habacker@freenet.de>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 #
@@ -16,8 +16,23 @@ if (WIN32)
     # opens the related .sln file with paths setting specified at 
     # configure time. 
     #
-    MACRO (addExplorerWrapper __projectname)
-        set (_projectname ${CMAKE_PROJECT_NAME})
+    # syntax: 
+    #   addExplorerWrapper()             - uses projectname set by project()    
+    #   addExplorerWrapper(project-name) - create wrapper with specific project-name 
+    #
+    # This macro requires FindPSDK.cmake for detecting platform sdk installation
+    #
+    MACRO (addExplorerWrapper)
+        set (_projectname ${arg1})
+        if (NOT _projectname)
+            set (_projectname ${CMAKE_PROJECT_NAME})
+        endif (NOT _projectname)
+        if (NOT _projectname)
+            set (_projectname "default")
+        endif (NOT _projectname)
+
+        set (SCRIPT_NAME "${CMAKE_BINARY_DIR}/${_projectname}-shell.bat")
+
         # write explorer wrappers
         get_filename_component(CMAKE_BIN_PATH ${CMAKE_COMMAND} PATH)
         set (ADD_PATH "${CMAKE_BIN_PATH}")
@@ -32,7 +47,7 @@ if (WIN32)
         if (MINGW)
             get_filename_component(MINGW_BIN_PATH ${CMAKE_CXX_COMPILER} PATH)
             set (ADD_PATH "${ADD_PATH};${MINGW_BIN_PATH}")
-            write_file (${CMAKE_BINARY_DIR}/${_projectname}-shell.bat "set PATH=${ADD_PATH};%PATH%\ncmd.exe")
+            write_file (${SCRIPT_NAME} "set PATH=${ADD_PATH};%PATH%\ncmd.exe")
         else (MINGW)
             find_package(PSDK)
             if (PSDK_INIT_SCRIPT)
@@ -48,9 +63,9 @@ if (WIN32)
 
             if (CMAKE_BUILD_TOOL STREQUAL  "nmake")
                 get_filename_component(VC_BIN_PATH ${CMAKE_CXX_COMPILER} PATH)
-                write_file (${CMAKE_BINARY_DIR}/${_projectname}-shell.bat "set PATH=${ADD_PATH};%PATH%\n${VC_INIT_CALL}${PSDK_INIT_CALL}cmd.exe")
+                write_file (${SCRIPT_NAME} "set PATH=${ADD_PATH};%PATH%\n${VC_INIT_CALL}${PSDK_INIT_CALL}cmd.exe")
             else (CMAKE_BUILD_TOOL STREQUAL  "nmake")
-                write_file (${CMAKE_BINARY_DIR}/${_projectname}-sln.bat "set PATH=${ADD_PATH};%PATH%\n${PSDK_INIT_CALL}start ${_projectname}.sln")
+                write_file (${SCRIPT_NAME} "set PATH=${ADD_PATH};%PATH%\n${PSDK_INIT_CALL}start ${_projectname}.sln")
             endif (CMAKE_BUILD_TOOL STREQUAL  "nmake")
         endif (MINGW)
     ENDMACRO (addExplorerWrapper)
