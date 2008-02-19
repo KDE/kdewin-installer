e158f13f0602e56bdb78886451c51f6dd36e6213
Ralf Habacker <ralf.habacker@freenet.de>
2008-02-19 09:16:09 +0000

    - added mirror list file generating
    svn path=/trunk/kdesupport/kdewin-installer/; revision=776989

---
+++ mirrors.lst.cmake
@@ -0,0 +1,33 @@
+http://downloads.sourceforge.net/kde-windows;downloads.sourceforge.net;;World Wide;
+http://gd.tuwien.ac.at/kde@RELEASE_PATH@;gd.tuwien.ac.at @RELEASE@;Europe;Austria
+http://ftp.scarlet.be/pub/kde@RELEASE_PATH@;ftp.scarlet.be @RELEASE@;Europe;Belgium;
+http://mirrors.dotsrc.org/kde@RELEASE_PATH@;mirrors.dotsrc.org  @RELEASE@;Europe;Denmark
+http://ftp-stud.fht-esslingen.de/Mirrors/ftp.kde.org/pub/kde@RELEASE_PATH@;ftp-stud.fht-esslingen.de  @RELEASE@;Europe;Germany;
+http://ftp.gwdg.de/pub/x11/kde@RELEASE_PATH@;ftp.gwdg.de @RELEASE@;Europe;Germany;
+http://www-ftp.lip6.fr/pub/X11/kde@RELEASE_PATH@;www-ftp.lip6.fr @RELEASE@;Europe;France;
+http://fr2.rpmfind.net/linux/KDE@RELEASE_PATH@;fr2.rpmfind.net @RELEASE@;Europe;France;
+http://kde-mirror.freenux.org@RELEASE_PATH@;kde-mirror.freenux.org @RELEASE@;Europe;France;
+http://ftp.funet.fi/pub/mirrors/ftp.kde.org/pub/kde@RELEASE_PATH@;ftp.funet.fi @RELEASE@;Europe;Finland;
+http://ftp.unina.it/pub/Linux/kde@RELEASE_PATH@;ftp.unina.it @RELEASE@;Europe;Italy;
+http://ftp.ntua.gr/pub/X11/kde@RELEASE_PATH@;ftp.ntua.gr @RELEASE@;Europe;Greece;
+http://ftp.heanet.ie/mirrors/ftp.kde.org@RELEASE_PATH@;ftp.heanet.ie @RELEASE@;Europe;Ireland;
+http://ftp.esat.net/mirrors/ftp.kde.org/pub/kde@RELEASE_PATH@;ftp.esat.net @RELEASE@;Europe;Ireland
+http://ftp.tuniv.szczecin.pl/pub/kde@RELEASE_PATH@;ftp.tuniv.szczecin.pl @RELEASE@;Europe;Poland;
+http://sunsite.icm.edu.pl/pub/unix/kde@RELEASE_PATH@;sunsite.icm.edu.pl @RELEASE@;Europe;Poland;
+http://mirrors.evolva.ro/kdeftp@RELEASE_PATH@;mirrors.evolva.ro @RELEASE@;Europe;Romania
+#http://www.mirrorservice.org/sites/ftp.kde.org/pub/kde@RELEASE_PATH@;www.mirrorservice.org @RELEASE@;Europe:United Kingdom;
+http://ftp.gtlib.cc.gatech.edu/pub/kde@RELEASE_PATH@;ftp.gtlib.cc.gatech.edu @RELEASE@;North America;United States;
+http://kde.mirrors.tds.net/pub/kde@RELEASE_PATH@;kde.mirrors.tds.net @RELEASE@;North America;United States;
+http://ftp.ussg.iu.edu/kde@RELEASE_PATH@;ftp.ussg.iu.edu @RELEASE@;North America;United States;
+http://mirrors.isc.org/pub/kde@RELEASE_PATH@;mirrors.isc.org @RELEASE@;North America;United States;
+http://mirror.cc.columbia.edu/pub/software/kde@RELEASE_PATH@;mirror.cc.columbia.edu @RELEASE@;North America;United States;
+http://mirror.csclub.uwaterloo.ca/kde@RELEASE_PATH@;mirror.csclub.uwaterloo.ca @RELEASE@;North America;Canada;
+http://ftp.rhnet.is/pub/kde@RELEASE_PATH@;ftp.rhnet.is @RELEASE@;Iceland;Iceland;
+http://mirror.pacific.net.au/kde@RELEASE_PATH@;mirror.pacific.net.au @RELEASE@;Asia & Oceania;Australia;
+http://public.planetmirror.com/pub/kde@RELEASE_PATH@;public.planetmirror.com @RELEASE@;Asia & Oceania;Australia;
+http://www.qtopia.org.cn/ftp/mirror/ftp.kde.org@RELEASE_PATH@;www.qtopia.org.cn @RELEASE@;Asia & Oceania;China;
+http://ftp.ring.gr.jp/pub/X/kde@RELEASE_PATH@;ftp.ring.gr.jp @RELEASE@;Asia & Oceania;Japan;
+http://ftp.kddlabs.co.jp/pub/X11/kde@RELEASE_PATH@;ftp.kddlabs.co.jp @RELEASE@;Asia & Oceania;Japan;
+http://ftp.yz.yamagata-u.ac.jp/pub/X11/wm/kde@RELEASE_PATH@;ftp.yz.yamagata-u.ac.jp @RELEASE@;Asia & Oceania;Japan;
+http://ftp.sayclub.com/pub/X/KDE@RELEASE_PATH@;ftp.sayclub.com @RELEASE@;Asia & Oceania;Republic of Korea;
+http://ftp.twaren.net/Unix/X/KDE@RELEASE_PATH@;ftp.twaren.net @RELEASE@;Asia & Oceania;Thaiwan;
