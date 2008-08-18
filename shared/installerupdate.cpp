46694315c5bbdfd1ffd0d1e2a8610c095c5dddc5
Ralf Habacker <ralf.habacker@freenet.de>
2008-08-18 09:17:58 +0000

    added new files
    svn path=/trunk/kdesupport/kdewin-installer/; revision=848621

---
+++ shared/installerupdate.cpp
@@ -0,0 +1,117 @@
+/****************************************************************************
+**
+** Copyright (C) 2006-2008 Ralf Habacker <ralf.habacker@freenet.de>. 
+** All rights reserved.
+**
+** This file is part of the KDE installer for windows
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Library General Public
+** License version 2 as published by the Free Software Foundation.
+**
+** This library is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+** Library General Public License for more details.
+**
+** You should have received a copy of the GNU Library General Public License
+** along with this library; see the file COPYING.LIB.  If not, write to
+** the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+** Boston, MA 02110-1301, USA.
+**
+****************************************************************************/
+
+#include "downloader.h"
+#include "downloaderprogress.h"
+#include "InstallerUpdate.h"
+#include "settings.h"
+
+#include <windows.h>
+
+#include <QCoreApplication>
+#include <QtDebug>
+#include <QProcess>
+#include <QFileInfo>
+
+InstallerUpdate::InstallerUpdate()
+{
+}
+
+InstallerUpdate::~InstallerUpdate()
+{
+}
+
+bool InstallerUpdate::isUpdateAvailable()
+{
+    return m_url.isValid() 
+        && !m_newVersion.isEmpty()
+        && !m_currentVersion.isEmpty() 
+        && m_newVersion != m_currentVersion;
+}
+
+void InstallerUpdate::setUrl(const QUrl &url)
+{
+    m_url = url;
+    char installerExePath[MAX_PATH+1];
+    GetModuleFileNameA(NULL, installerExePath, MAX_PATH);
+    QFileInfo pi(installerExePath); 
+    QFileInfo fi(m_url.path()); 
+    m_localFilePath = pi.absolutePath() + "/" + fi.fileName();
+}
+
+bool InstallerUpdate::fetch()
+{
+    if (m_localFilePath.isEmpty() || !m_url.isValid())
+    {
+        qCritical() << "no url or local file path set";
+        return false;
+    }
+    if (m_url.scheme() == "file")
+    {
+        QFile a(m_url.toLocalFile());
+        if (!a.exists())
+        {
+            qCritical() << "could not find file to copy" << m_url.toLocalFile();
+            return false;
+        }
+        if (!a.copy(m_localFilePath)) 
+        {
+            qCritical() << "could not copy file from" << m_url.toLocalFile() << "to" << m_localFilePath;
+            return false;
+        }
+        return true;
+    }
+    else if (!Downloader::instance()->fetch(m_url,m_localFilePath))
+    {
+        qCritical() << "could not fetch new installer" << m_url;
+        return false;
+    }
+    return true;
+}
+
+bool InstallerUpdate::run()
+{
+    if (m_url.isValid())
+    {
+        QProcess::startDetached(m_localFilePath);
+        QCoreApplication::quit();
+        return true;
+    }
+    return false;
+}
+
+InstallerUpdate &InstallerUpdate::instance()
+{
+    static InstallerUpdate InstallerUpdate;
+    return InstallerUpdate;
+}
+
+QDebug &operator<<(QDebug &out, const InstallerUpdate &c)
+{
+    out << "InstallerUpdate ("
+        << "m_url:" << c.m_url
+        << "m_newVersion:" << c.m_newVersion
+        << "m_currentVersion:" << c.m_currentVersion
+        << ")";
+    return out;
+}
---
+++ shared/installerupdate.h
@@ -0,0 +1,61 @@
+/****************************************************************************
+**
+** Copyright (C) 2006-2007 Ralf Habacker. All rights reserved.
+**
+** This file is part of the KDE installer for windows
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Library General Public
+** License version 2 as published by the Free Software Foundation.
+**
+** This library is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+** Library General Public License for more details.
+**
+** You should have received a copy of the GNU Library General Public License
+** along with this library; see the file COPYING.LIB.  If not, write to
+** the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+** Boston, MA 02110-1301, USA.
+**
+****************************************************************************/
+#ifndef InstallerUpdate_H
+#define InstallerUpdate_H
+
+#include <QtCore/QString>
+#include <QtCore/QUrl>
+
+/**    
+   \brief The InstallerUpdate class provides access to new installer versions.
+    */
+class InstallerUpdate
+{
+    public:
+        InstallerUpdate();
+        ~InstallerUpdate();
+
+        /// set current installer version
+        void setCurrentVersion(const QString version) { m_currentVersion = version; }
+        /// set new installer version
+        void setNewVersion(const QString version) { m_newVersion = version; }
+        /// set url for new update
+        void setUrl(const QUrl &url);
+        /// check if the provided version is newer 
+        bool isUpdateAvailable();
+        /// fetch new installer 
+        bool fetch();
+
+        /// run new installer 
+        bool run();
+        /// return singleton instance
+        static InstallerUpdate &instance();
+    protected:
+        QUrl m_url;
+        QString m_newVersion;
+        QString m_currentVersion;
+        QString m_localFilePath;
+    friend QDebug &operator<<(QDebug &out, const InstallerUpdate &c);
+};
+
+
+#endif
