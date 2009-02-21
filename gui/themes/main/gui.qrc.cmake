e8b722e1293f8a9c2454a9e2d4cd96978ccb1ceb
Ralf Habacker <ralf.habacker@freenet.de>
2009-02-21 17:53:08 +0000

    added kde4 desktop file to have the installer in the startmenu
    svn path=/trunk/kdesupport/kdewin-installer/; revision=929605

---
+++ gui/themes/main/gui.qrc.cmake
@@ -0,0 +1,15 @@
+ <!DOCTYPE RCC><RCC version="1.0">
+ <qresource>
+     <file alias="autoinstall.xpm">${CMAKE_CURRENT_SOURCE_DIR}/autoinstall.xpm</file>
+     <file alias="del.xpm">${CMAKE_CURRENT_SOURCE_DIR}/del.xpm</file>
+     <file alias="install.xpm">${CMAKE_CURRENT_SOURCE_DIR}/install.xpm</file>
+     <file alias="keepinstalled.xpm">${CMAKE_CURRENT_SOURCE_DIR}/keepinstalled.xpm</file>
+     <file alias="noinst.xpm">${CMAKE_CURRENT_SOURCE_DIR}/noinst.xpm</file>
+     <file alias="update.xpm">${CMAKE_CURRENT_SOURCE_DIR}/update.xpm</file>
+     <file alias="watermark.png">${CMAKE_CURRENT_SOURCE_DIR}/watermark.png</file>
+     <file alias="banner.png">${CMAKE_CURRENT_SOURCE_DIR}/banner.png</file>
+     <file alias="logo.png">${CMAKE_CURRENT_SOURCE_DIR}/logo.png</file>
+     <file alias="installer.desktop">${CMAKE_CURRENT_BINARY_DIR}/installer.desktop</file>
+ </qresource>
+ </RCC>
+
---
+++ gui/themes/main/installer.desktop.cmake
@@ -0,0 +1,10 @@
+[Desktop Entry]
+Type=Application
+Exec=${TARGET_FILENAME}
+Icon=kde
+# not supported yet
+#X-DocPath=${APPNAME}-installer-gui/index.html
+Name=${APPNAME}-installer-gui
+Name[x-test]=xx${APPNAME}-installer-guixx
+Terminal=false
+Categories=Qt;KDE;Core;
