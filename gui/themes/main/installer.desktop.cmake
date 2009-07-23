937aaa9013c52c5bc9972dcf07d9d42ae81d7eec
Ralf Habacker <ralf.habacker@freenet.de>
2009-07-23 21:48:49 +0000

    update installer name in start menu
    svn path=/trunk/kdesupport/kdewin-installer/; revision=1001663

--- gui/themes/main/installer.desktop.cmake
+++ gui/themes/main/installer.desktop.cmake
@@ -3,8 +3,8 @@ Type=Application
 Exec=${TARGET_FILENAME}
 Icon=kde
 # not supported yet
-#X-DocPath=${APPNAME}-installer-gui/index.html
-Name=${APPNAME}-installer-gui
-Name[x-test]=xx${APPNAME}-installer-guixx
+#X-DocPath=${APPNAME}-installer/index.html
+Name=${DESKTOP_APPNAME}
+Name[x-test]=xx${DESKTOP_APPNAME}
 Terminal=false
 Categories=Qt;KDE;Core;
