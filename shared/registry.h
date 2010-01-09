244654f95c005f9031d8cb4eed802bb5268e12e9
Patrick Spendrin <ps_ml@gmx.de>
2010-01-09 01:36:20 +0000

    make typedefs work also under Linux
    svn path=/trunk/kdesupport/kdewin-installer/; revision=1071929

--- shared/registry.h
+++ shared/registry.h
@@ -28,8 +28,9 @@
 #include <QString>
 #include <QVariant>
 
-typedef enum RegKey { hKEY_CURRENT_USER, hKEY_LOCAL_MACHINE, hKEY_CLASSES_ROOT };
-typedef enum RegType { qt_unknown = -1, qt_String, qt_ExpandedString, qt_DWORD, qt_BINARY, qt_MultiString };
+typedef enum { hKEY_CURRENT_USER, hKEY_LOCAL_MACHINE, hKEY_CLASSES_ROOT }  RegKey;
+typedef enum { qt_unknown = -1, qt_String, qt_ExpandedString, qt_DWORD, qt_BINARY, qt_MultiString } RegType;
+
 QVariant getWin32RegistryValue(RegKey akey, const QString& subKey, const QString& item, bool *ok = false);
 bool setWin32RegistryValue(const RegKey& akey, const QString& subKey, const QString& item, const QVariant& value, RegType pqvType = qt_unknown);
 bool delWin32RegistryValue(const RegKey& akey, const QString& subKey);
