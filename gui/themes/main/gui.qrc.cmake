268b4ea8f0cbea195591ba677d1a230ff7ce3bff
Patrick Spendrin <ps_ml@gmx.de>
2010-01-15 02:44:52 +0000

    add an icon to make it possible to show half-filled meta packages
    svn path=/trunk/kdesupport/kdewin-installer/; revision=1074973

--- gui/themes/main/gui.qrc.cmake
+++ gui/themes/main/gui.qrc.cmake
@@ -6,6 +6,7 @@
      <file alias="keepinstalled.xpm">${CMAKE_CURRENT_SOURCE_DIR}/keepinstalled.xpm</file>
      <file alias="noinst.xpm">${CMAKE_CURRENT_SOURCE_DIR}/noinst.xpm</file>
      <file alias="update.xpm">${CMAKE_CURRENT_SOURCE_DIR}/update.xpm</file>
+     <file alias="dirty.xpm">${CMAKE_CURRENT_SOURCE_DIR}/dirty.xpm</file>
      <file alias="watermark.png">${CMAKE_CURRENT_SOURCE_DIR}/watermark.png</file>
      <file alias="banner.png">${CMAKE_CURRENT_SOURCE_DIR}/banner.png</file>
      <file alias="logo.png">${CMAKE_CURRENT_SOURCE_DIR}/logo.png</file>
