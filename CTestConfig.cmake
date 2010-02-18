9bd276604aac8f9e97b952f0b18a3cdb132841b5
Patrick Spendrin <ps_ml@gmx.de>
2010-02-18 16:41:13 +0000

    add a way to build nightly builds and submit them to my.cdash.org
    svn path=/trunk/kdesupport/kdewin-installer/; revision=1092253

---
+++ CTestConfig.cmake
@@ -0,0 +1,20 @@
+## This file should be placed in the root directory of your project.
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+## # The following are required to uses Dart and the Cdash dashboard
+##   ENABLE_TESTING()
+##   INCLUDE(Dart)
+set(CTEST_PROJECT_NAME "kdewin-installer")
+set(CTEST_NIGHTLY_START_TIME "18:00:00 CET")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=kdewin-installer")
+set(CTEST_DROP_SITE_CDASH TRUE)
+
+set(CTEST_CUSTOM_MAXIMUM_NUMBER_OF_WARNINGS 10000)
+set(CTEST_CUSTOM_WARNING_EXCEPTION "Warning: No relevant classes found. No output generated."  # moc didn't find anything to do
+                                   "yy" "YY"                                                   # ignore the warnings in generated code
+   )
+set(CTEST_CUSTOM_ERROR_EXCEPTION "ICECC")
+set(CTEST_CUSTOM_COVERAGE_EXCLUDE ".moc$" "moc_" "ui_")
