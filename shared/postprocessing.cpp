c8e32d75e998623b238cbc33db9e662e5c0116fd
Ralf Habacker <ralf.habacker@freenet.de>
2010-07-30 15:59:13 +0000

    added new class PostProcessing which handle post processing stuff
    svn path=/trunk/kdesupport/kdewin-installer/; revision=1157277

---
+++ shared/postprocessing.cpp
@@ -0,0 +1,60 @@
+/****************************************************************************
+**
+** Copyright (C) 2005-2010 Ralf Habacker. All rights reserved.
+**
+** This file is part of the KDE installer for windows
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Library General Public
+** License version 2 as published by the Free Software Foundation.
+**
+** This library is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+** Library General Public License for more details.
+**
+** You should have received a copy of the GNU Library General Public License
+** along with this library; see the file COPYING.LIB.  If not, write to
+** the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+** Boston, MA 02110-1301, USA.
+**
+****************************************************************************/
+
+#include "postprocessing.h"
+#include "settings.h"
+
+#include <QListWidget>
+#include <QProcess>
+
+PostProcessing::PostProcessing(QObject *parent) : QObject(parent)
+{
+}
+
+bool PostProcessing::runCommand(int index, const QString &msg, const QString &app, const QStringList &params)
+{
+    QFileInfo f(Settings::instance().installDir()+"/bin/" + app + ".exe");
+    qDebug() << "checking for app " << app << " - "  <<(f.exists() ? "found" : "not found");
+    if (!f.exists())
+        return false;
+
+    emit commandStarted(index);
+    emit commandStarted(msg);
+
+    qDebug() << "running " << app << params;
+    int ret = QProcess::execute( f.absoluteFilePath(), params);
+    emit commandFinished(ret == 0);
+    return ret == 0;
+}
+
+bool PostProcessing::run()
+{
+    emit numberOfCommands(5);
+    runCommand(0,"updating mime database","update-mime-database",QStringList() << QDir::fromNativeSeparators(Settings::instance().installDir()) + "/share/mime");
+    runCommand(1,"starting kde background processes","kdeinit4");
+    //runCommand(1,"updating system configuration database","kbuildsycoca4");
+    //runCommand(2,"deleting old windows start menu entries","kwinstartmenu",QStringList() <<  "--remove");
+    //runCommand(3,"creating new windows start menu entries","kwinstartmenu");
+    //runCommand(4,"stopping kde background processes","kdeinit4", QStringList() << "--terminate");
+    emit finished();
+    return true;
+}
---
+++ shared/postprocessing.h
@@ -0,0 +1,52 @@
+/****************************************************************************
+**
+** Copyright (C) 2005-2010 Ralf Habacker. All rights reserved.
+**
+** This file is part of the KDE installer for windows
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Library General Public
+** License version 2 as published by the Free Software Foundation.
+**
+** This library is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+** Library General Public License for more details.
+**
+** You should have received a copy of the GNU Library General Public License
+** along with this library; see the file COPYING.LIB.  If not, write to
+** the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+** Boston, MA 02110-1301, USA.
+**
+****************************************************************************/
+
+#ifndef POSTPROCESSING_H
+#define POSTPROCESSING_H
+
+#include <QObject>
+#include <QStringList>
+
+/**
+ handle postprocessing 
+*/ 
+class PostProcessing : public QObject
+{
+    Q_OBJECT
+public:
+    PostProcessing(QObject *parent = 0);
+    bool run();
+
+signals:
+    void numberOfCommands(int count);
+    void commandStarted(int index);
+    void commandStarted(const QString &text);
+    void commandFinished(bool failed);
+    void finished();
+
+public slots:
+
+protected:
+    bool runCommand(int index, const QString &msg, const QString &app, const QStringList &params=QStringList());
+};
+
+#endif // POSTPROCESSING_H
