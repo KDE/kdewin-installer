e2d22b4de8e5d64fa829649997069bd61a19e6b8
Ralf Habacker <ralf.habacker@freenet.de>
2010-08-04 12:49:50 +0000

    svn path=/trunk/kdesupport/kdewin-installer/; revision=1159119

---
+++ single-package-installer/installerdialog.cpp
@@ -0,0 +1,246 @@
+/****************************************************************************
+**
+** Copyright (C) 2010 Ralf Habacker ralf.habacker@freenet.de
+**
+** This file is part of the KDE installer for windows
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Library General Public
+** License version 2 as published by the Free Software Foundation.
+**
+** This library is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+** Library General Public License for more details.
+**
+** You should have received a copy of the GNU Library General Public License
+** along with this library; see the file COPYING.LIB.  If not, write to
+** the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+** Boston, MA 02110-1301, USA.
+**
+****************************************************************************/
+
+#include "installerdialog.h"
+#include "installerenginesinglepackage.h"
+
+#include "../shared/misc.h"
+#include "../shared/debug.h"
+#include "../shared/installercallconfig.h"
+#include "../shared/installerprogress.h"
+#include "../shared/downloader.h"
+#include "../shared/unpacker.h"
+#include "../shared/postprocessing.h"
+
+#include <QtGui>
+
+InstallerEngineSinglePackage engine;
+
+InstallerDialog::InstallerDialog()
+    :  okay(":/images/dialog-ok-apply.png"), next(":/images/go-next.png"), error(":/images/dialog-cancel.png")
+{
+    ui.setupUi(this);
+
+    connect(ui.closeButton, SIGNAL(clicked()), this, SLOT(stop()));
+    initItems();
+
+    setMessageHandler();
+
+    QString setupName;
+
+    if (QCoreApplication::arguments().size() > 1)
+        setupName = QCoreApplication::arguments().at(1);
+
+    InstallerCallConfig config(setupName);
+
+    if (config.isValid())
+    {
+        InstallerEngine::defaultConfigURL = QString("http://www.winkde.org/pub/kde/ports/win32/releases/%1/%2").arg(toString(config.releaseType)).arg(config.version);
+        QString installRoot = QString("%1/KDE-%2-%3-%4").arg(QLatin1String(qgetenv("ProgramFiles"))).arg(toString(config.compilerType)).arg(toString(config.releaseType)).arg(config.version);
+        addHint("I'm installing into " + installRoot);
+        engine.setRoot(installRoot);
+        ProxySettings ps;
+
+        setWindowTitle(tr("KDE %1 Application Installer").arg(config.packageName));
+        ui.topLabel->setText(tr("KDE %1 Installer").arg(config.packageName));
+
+        if (ps.from(ProxySettings::AutoDetect))
+        {
+            ps.save();
+            addHint("I'm " + ps.toString());
+        }
+        packages << config.packageName+'-'+toString(config.compilerType);
+        setItem(0);
+        QTimer::singleShot(250,this,SLOT(setupEngine()));
+    }
+    else
+    {
+        setWindowTitle(tr("KDE Single Application Installer"));
+        ui.topLabel->setText(tr("KDE Single Application Installer"));
+        addHint("I could not found a valid configuration");
+    }
+}
+
+void InstallerDialog::initItems()
+{
+    ui.label1->setText("");
+    ui.label1->setVisible(false);
+    ui.textLabel1->setVisible(false);
+    ui.label2->setText("");
+    ui.label2->setVisible(false);
+    ui.textLabel2->setVisible(false);
+    ui.label3->setText("");
+    ui.label3->setVisible(false);
+    ui.textLabel3->setVisible(false);
+    ui.label4->setText("");
+    ui.label4->setVisible(false);
+    ui.textLabel4->setVisible(false);
+    ui.label5->setText("");
+    ui.label5->setVisible(false);
+    ui.textLabel5->setVisible(false);
+}
+
+void InstallerDialog::addHint(const QString &hint)
+{
+    ui.hintLabel->setText(ui.hintLabel->text() + "\n" + hint);
+    QCoreApplication::processEvents();
+}
+
+void InstallerDialog::setItem(int pagenum)
+{
+    switch (pagenum)
+    {
+        case 0:
+            ui.label1->setPixmap(next);
+            ui.label1->setVisible(true);
+            ui.textLabel1->setVisible(true);
+            break;
+
+        case 1:
+            ui.label1->setPixmap(okay);
+            ui.label2->setPixmap(next);
+            ui.label2->setVisible(true);
+            ui.textLabel2->setVisible(true);
+            break;
+
+        case 2:
+            ui.label2->setPixmap(okay);
+            ui.label3->setPixmap(next);
+            ui.label3->setVisible(true);
+            ui.textLabel3->setVisible(true);
+            break;
+
+        case 3:
+            ui.label3->setPixmap(okay);
+            ui.label4->setPixmap(next);
+            ui.label4->setVisible(true);
+            ui.textLabel4->setVisible(true);
+            break;
+
+        case 4:
+            ui.label4->setPixmap(okay);
+            ui.label5->setPixmap(next);
+            ui.label5->setVisible(true);
+            ui.textLabel5->setVisible(true);
+            break;
+
+        case 5:
+            ui.label5->setPixmap(okay);
+            break;
+    }
+    QCoreApplication::processEvents();
+}
+
+void InstallerDialog::setError(int pagenum)
+{
+    switch (pagenum)
+    {
+        case 0:
+            ui.label1->setPixmap(error);
+            break;
+
+        case 1:
+            ui.label2->setPixmap(error);
+            break;
+
+        case 2:
+            ui.label3->setPixmap(error);
+            break;
+
+        case 3:
+            ui.label4->setPixmap(error);
+            break;
+
+        case 4:
+            ui.label5->setPixmap(error);
+            break;
+    }
+    QCoreApplication::processEvents();
+}
+
+void InstallerDialog::setupEngine()
+{  
+    setItem(1);
+    addHint("I'm fetching packages from " + InstallerEngine::defaultConfigURL);
+    if (engine.init())
+    {
+        Package *p = engine.getPackageByName(packages[0]);
+        packagesToInstall.append(p);
+        engine.setDependencyState(p,packagesToInstall);
+        QTimer::singleShot(1,this,SLOT(downloadPackages()));
+    }
+    else
+    {
+        addHint(QString(".... initialisation failed"));
+        setError(1);
+    }
+}
+
+void InstallerDialog::downloadPackages()
+{
+    setItem(2);
+    addHint(QString("I'm downloading up to %1 package depending on what downloaded earlier").arg(packagesToInstall.size()));
+    if (engine.downloadPackages(packagesToInstall))
+        QTimer::singleShot(1,this,SLOT(installPackages()));
+    else
+    {
+        addHint(QString(".... download failed"));
+        setError(2);
+    }
+}
+
+void InstallerDialog::installPackages()
+{
+    setItem(3);
+    addHint(QString("I'm installing %1 package(s)").arg(packagesToInstall.size()));
+    if (engine.installPackages(packagesToInstall))
+        QTimer::singleShot(1,this,SLOT(postProcessing()));
+    else
+    {
+        addHint(QString(".... installation failed"));
+        setError(3);
+    }
+}
+
+void InstallerDialog::postProcessing()
+{
+    setItem(4);
+    addHint("I'm running tools:");
+    connect(&m_postProcessing,SIGNAL(finished()),this,SLOT(finished()));
+    connect(&m_postProcessing,SIGNAL(commandStarted(const QString &)),this,SLOT(addHint(const QString &)));
+    ui.closeButton->setEnabled(false);
+    m_postProcessing.start();
+}
+
+void InstallerDialog::finished()
+{
+    setItem(5);
+    ui.closeButton->setEnabled(true);
+    ui.closeButton->setText(tr("Finish"));
+}
+
+void InstallerDialog::stop()
+{
+    m_postProcessing.stop();
+    QDialog::close();
+}
+
---
+++ single-package-installer/installerdialog.h
@@ -0,0 +1,72 @@
+/****************************************************************************
+**
+** Copyright (C) 2010 Ralf Habacker ralf.habacker@freenet.de
+**
+** This file is part of the KDE installer for windows
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Library General Public
+** License version 2 as published by the Free Software Foundation.
+**
+** This library is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+** Library General Public License for more details.
+**
+** You should have received a copy of the GNU Library General Public License
+** along with this library; see the file COPYING.LIB.  If not, write to
+** the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+** Boston, MA 02110-1301, USA.
+**
+****************************************************************************/
+
+#ifndef INSTALLERDIALOG_H
+#define INSTALLERDIALOG_H
+
+#include "ui_installerdialog.h"
+
+#include "../shared/package.h"
+#include "../shared/postprocessing.h"
+
+
+
+#include <QDialog>
+
+class QPixmap;
+
+class InstallerDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    InstallerDialog();
+
+protected slots:
+    void setupEngine();
+    void downloadPackages();
+    void installPackages();
+    void postProcessing();
+    void finished();
+
+protected:
+    Ui::InstallerDialog ui;
+
+protected slots:
+    void addHint(const QString &hint);
+    void stop();
+
+private:
+    void initItems();
+    void setItem(int pagenum);
+    void setError(int pagenum);
+
+    QStringList packages;
+    QList<Package *> packagesToInstall;
+    PostProcessing m_postProcessing;
+    int page;
+    QPixmap next;
+    QPixmap okay;
+    QPixmap error;
+};
+
+#endif
