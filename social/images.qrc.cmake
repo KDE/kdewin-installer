232abb242944b9325ffc78740d5e3b24f2a2cae0
Alexandru Tudorica <tudalex@gmail.com>
2011-08-20 20:42:59 +0300

    Added images to categories.

---
+++ social/images.qrc.cmake
@@ -0,0 +1,17 @@
+ <!DOCTYPE RCC><RCC version="1.0">
+    <qresource prefix="/">
+        <file alias="kde-logo.png">${CMAKE_CURRENT_SOURCE_DIR}/images/kde-logo.png</file>
+        <file alias="kde-logo.png">${CMAKE_CURRENT_SOURCE_DIR}/images/kde-logo.png</file>
+        <file alias="Development.png">${CMAKE_CURRENT_SOURCE_DIR}/images/applications-development.png</file>
+        <file alias="Education.png">${CMAKE_CURRENT_SOURCE_DIR}/images/applications-education.png</file>
+        <file alias="Games.png">${CMAKE_CURRENT_SOURCE_DIR}/images/applications-games.png</file>
+        <file alias="Graphics.png">${CMAKE_CURRENT_SOURCE_DIR}/images/applications-graphics.png</file>
+        <file alias="Internet.png">${CMAKE_CURRENT_SOURCE_DIR}/images/applications-internet.png</file>
+        <file alias="Multimedia.png">${CMAKE_CURRENT_SOURCE_DIR}/images/applications-multimedia.png</file>
+        <file alias="Office.png">${CMAKE_CURRENT_SOURCE_DIR}/images/applications-office.png</file>
+        <file alias="System.png">${CMAKE_CURRENT_SOURCE_DIR}/images/applications-system.png</file>
+        <file alias="Utilities.png">${CMAKE_CURRENT_SOURCE_DIR}/images/applications-utilities.png</file>
+        <file alias="Comments.png">${CMAKE_CURRENT_SOURCE_DIR}/images/comments.png</file>
+        <file alias="Install.png">${CMAKE_CURRENT_SOURCE_DIR}/images/install.png</file>
+    </qresource>
+</RCC>
