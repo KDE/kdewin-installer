a38ba4ce09b16e26e499a6e04a6cbba2d483f4ee
Ralf Habacker <ralf.habacker@freenet.de>
2017-03-23 19:00:37 +0100

    Remove obsolate QT5_WRAP_CPP calls because we use CMAKE_AUTOMOC.

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -144,10 +144,6 @@ macro (add_themed_installer _appname _resource)
   endif(MINGW)
   endif(WIN32)
 
-  if(QT5_FOUND)
-    QT5_WRAP_CPP(all_sources ${UI_SOURCES})
-  endif()
-
 #  if (BUILD_TRANSLATIONS)
 #      qt4_create_translation(
 #          _qm_files
