3211a1076024dc7181bab1a6bc5df08e1bc6e02c
Ralf Habacker <ralf.habacker@freenet.de>
2017-03-31 12:44:22 +0200

    Complete fix of packagein qt symbol package.

--- packager/packager.cpp
+++ packager/packager.cpp
@@ -219,10 +219,16 @@ bool Packager::generatePackageFileList(QList<InstallFile> &fileList, Packager::T
                 generateFileList(fileList, dir, QString(), "*.xml", "");
                 return true;
             case DEBUG:
-                generateFileList(fileList, dir, "bin",  "*d4.dll *d.dll *d.pdb", "");
-                generateFileList(fileList, dir, "lib",  "d4*.pdb *d.pdb", "");
+                if (m_type.contains("vc"))
+                {
+                    generateFileList(fileList, dir, ".", "*.pdb");
+                }
+                else if (m_type.contains("mingw"))
+                {
+                    generateFileList(fileList, dir, ".", "*.sym");
+                }
+                generateFileList(fileList, dir, "bin",  "*d4.dll *d.dll", "");
                 generateFileList(fileList, dir, "plugins", "*d.dll *d4.dll *d1.dll");
-                generateFileList(fileList, dir, "plugins", "*d.pdb *d4.pdb *d1.pdb");
                 if (m_type == "mingw" || m_type == "mingw4")
                     generateFileList(fileList, dir, "lib",      "*d4.a *d.a");
                 else
