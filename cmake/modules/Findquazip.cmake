e2ac4e6c16ed5a35992c9de9d0ce1c8fddf05fd5
Ralf Habacker <ralf.habacker@freenet.de>
2020-07-29 19:39:29 +0200

    cmake: remove QUAZIP_ related variables because we use the target

--- cmake/modules/Findquazip.cmake
+++ cmake/modules/Findquazip.cmake
@@ -2,15 +2,18 @@
 # QUAZIP_INCLUDE_DIR - the QUAZIP include directory
 # QUAZIP_LIBRARIES - The libraries needed to use QUAZIP
 
-if(QUAZIP_INCLUDE_DIR AND QUAZIP_LIBRARIES)
-  set(QUAZIP_FOUND TRUE)
-else(QUAZIP_INCLUDE_DIR AND QUAZIP_LIBRARIES)
-
+if(NOT QUAZIP_INCLUDE_DIR)
   find_path(QUAZIP_INCLUDE_DIR NAMES quazip.h PATH_SUFFIXES quazip)
   find_library(QUAZIP_LIBRARIES NAMES quazip)
 
   include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(quazip DEFAULT_MSG QUAZIP_INCLUDE_DIR QUAZIP_LIBRARIES)
+  find_package_handle_standard_args(QUAZIP DEFAULT_MSG QUAZIP_INCLUDE_DIR QUAZIP_LIBRARIES)
 
   mark_as_advanced(QUAZIP_INCLUDE_DIR QUAZIP_LIBRARIES)
-endif(QUAZIP_INCLUDE_DIR AND QUAZIP_LIBRARIES)
+endif()
+
+if (QUAZIP_FOUND)
+    add_library(quazip UNKNOWN IMPORTED)
+    set_target_properties(quazip PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${QUAZIP_INCLUDE_DIR}")
+    set_target_properties(quazip PROPERTIES IMPORTED_LOCATION "${QUAZIP_LIBRARIES}")
+endif()
