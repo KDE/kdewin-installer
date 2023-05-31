c63594ddb6997e97bf8a76941f5de3b373223125
Ralf Habacker <ralf.habacker@freenet.de>
2023-05-31 16:40:18 +0200

    Move test data into a subdirectory specific to the related test

--- tests/test-hashfile.cpp
+++ tests/test-hashfile.cpp
@@ -15,17 +15,17 @@
 
 void TestHashFile::test1()
 {
-    HashFile hashFile(Hash::SHA1, "test-hashfile.txt", CMAKE_SOURCE_DIR "/tests/data/");
+    HashFile hashFile(Hash::SHA1, "test-hashfile.txt", CMAKE_SOURCE_DIR "/tests/data/hashfile");
     QCOMPARE(hashFile.computeHash(), true);
     QCOMPARE(QString(hashFile.toHashFileContent()), QString("e9506d89dd4b301b77ebd059df5faffea732b921  test-hashfile.txt\n"));
 
-    HashFile hashFile2(Hash::SHA1, CMAKE_SOURCE_DIR "/tests/data/test-hashfile.txt");
+    HashFile hashFile2(Hash::SHA1, CMAKE_SOURCE_DIR "/tests/data/hashfile/test-hashfile.txt");
     QCOMPARE(hashFile2.computeHash(), true);
-    QCOMPARE(QString(hashFile2.toHashFileContent()), QString("e9506d89dd4b301b77ebd059df5faffea732b921  " CMAKE_SOURCE_DIR "/tests/data/test-hashfile.txt\n"));
+    QCOMPARE(QString(hashFile2.toHashFileContent()), QString("e9506d89dd4b301b77ebd059df5faffea732b921  " CMAKE_SOURCE_DIR "/tests/data/hashfile/test-hashfile.txt\n"));
 
-    HashFile hashFile3(Hash::SHA1, CMAKE_SOURCE_DIR "/tests/data/test-hashfile space.txt");
+    HashFile hashFile3(Hash::SHA1, CMAKE_SOURCE_DIR "/tests/data/hashfile/test-hashfile space.txt");
     QCOMPARE(hashFile3.computeHash(), true);
-    QCOMPARE(QString(hashFile3.toHashFileContent()), QString("e9506d89dd4b301b77ebd059df5faffea732b921  " CMAKE_SOURCE_DIR "/tests/data/test-hashfile\\ space.txt\n"));
+    QCOMPARE(QString(hashFile3.toHashFileContent()), QString("e9506d89dd4b301b77ebd059df5faffea732b921  " CMAKE_SOURCE_DIR "/tests/data/hashfile/test-hashfile\\ space.txt\n"));
 }
 
 QTEST_MAIN(TestHashFile)
